6fad60bf42c572a400a342f4b3a836da79c604df
Shiham123 <s36651786@gmail.com>
2024-03-23 06:03:13 +0600

    color setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-	return <div className="text-5xl text-red-500">this is app</div>
+	return <div className="text-5xl text-rose">this is app</div>
 }
 
 export default App
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,24 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"light-bg-primary": "#e4f0fa",
+				"light-bg-secondary": "#e6eef8",
+				"blue-gray": "#2b3c5b",
+				rose: "#f8917e",
+				levender: "#978aa3",
+				indigo: "#4b4961",
+				// dark colors
+				"dark-bg-primary": "#121215",
+				"dark-bg-secondary": "#18181c",
+				"dark-primary": "#dfe0fb",
+				"dark-secondary": "#7c57f7",
+				"dark-font-primary": "#7f8fd2",
+				"dark-font-secondary": "#77848d",
+				"dark-icon-secondary": "#8887c5",
+			},
+		},
 	},
 	plugins: [],
 }
