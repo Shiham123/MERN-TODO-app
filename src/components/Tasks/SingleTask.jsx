1e32363e458a740e8c2ce1c68725563ca516774c
Shiham123 <s36651786@gmail.com>
2024-04-09 16:52:58 +0600

    task details page working

--- src/components/Tasks/SingleTask.jsx
+++ src/components/Tasks/SingleTask.jsx
@@ -1,14 +1,16 @@
 import {useSelector} from "react-redux"
 
 import TaskBodyHeading from "../../shared/TaskBodyHeading"
+import {RiStarFill, RiStarLine} from "react-icons/ri"
 
 const SingleTask = () => {
 	const {tasks} = useSelector((state) => state.formData)
 	const {perTaskId} = useSelector((state) => state.component)
+	const {enabled} = useSelector((state) => state.theme.themeList)
 
 	const foundTask = tasks.find((item) => item.id === perTaskId)
 
-	const {title, note} = foundTask
+	const {title, note, isStarred} = foundTask
 
 	return (
 		<div className="p-4">
@@ -16,7 +18,21 @@ const SingleTask = () => {
 			{foundTask && (
 				<>
 					<div>
-						<h1>{title}</h1>
+						<div className="flex justify-between items-center border-b-4 border-darkBgPrimary">
+							<h1
+								className={`font-Lexend text-2xl py-8 ${
+									enabled ? "text-lightBgPrimary" : "text-darkBgPrimary"
+								}`}
+							>
+								{title}
+							</h1>
+							{isStarred ? (
+								<RiStarFill className="cursor-pointer" color="#7a8db0" size={30} />
+							) : (
+								<RiStarLine className="cursor-pointer" color="#7a8db0" size={30} />
+							)}
+						</div>
+
 						<p>{note}</p>
 					</div>
 				</>
